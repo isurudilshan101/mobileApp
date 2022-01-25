bc5888da20a87e64b66c9e72e383c56aa1076609
isurudilshan101 <isuru.dilshan101@gmail.com>
2022-01-25 12:38:21 +0530

    setting

--- screen/setting/setting.js
+++ screen/setting/setting.js
@@ -1,28 +1,49 @@
 import React from "react";
-import { View, Text,StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import AppBar from "../../component/appBar";
 import SettingIcon from "react-native-vector-icons/Ionicons";
+// import { Button } from "@mui/material";
 
-const title = "Home";
+const title = "Setting";
 
 const Setting = () => {
   return (
     <View>
-      <AppBar title={title} />
-      <View style={styles.settingView}>
-        <View>
-          <SettingIcon name="settings" size={50} />
-        </View>
+      <View>
+        <AppBar title={title} />
       </View>
+
+      <View style={styles.settingView}>
+        <SettingIcon name="settings" size={50} />
+      </View>
+
+      <View style={styles.shereButton}>
+      <Button style={styles.buttonStyle} color={"blue"} title="Language" />
+      <Button style={styles.buttonStyle} color={"blue"} title="Privecy Policy" />
+      <Button style={styles.buttonStyle} color={"blue"} title="Feedback" />
+      <Button style={styles.buttonStyle} color={"blue"} title="Share" />
+
+
+      </View>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    settingView:{
-            alignItems: "center",
-            marginTop:40,
-    }
-})
+  settingView: {
+    alignItems: "center",
+    marginTop: 40,
+  },
+  shereButton: {
+      width:'50%',
+      marginLeft: 90,
+      marginTop:60,
+  },
+
+  buttonStyle:{
+      
+  }
+});
 
 export default Setting;
